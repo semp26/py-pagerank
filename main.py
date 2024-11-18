d43b353aa7115008fa1edfc4749ce05d470ffaba
steev <mauricefl@outlook.de>
2024-11-18 12:33:51 +0100

    created empty classes

---
+++ main.py
@@ -0,0 +1,19 @@
+# a link defines relation between 2 nodes
+class Link:
+    def __init__(self):
+        return
+
+# Colum houses nodes
+class Colum:
+    def __init__(self):
+        return
+
+# definition of a standard node
+class Node:
+    __col: [Colum]
+
+    def __init__(self):
+        return
+
+def main():
+    return
