e2af75720036482c59e281760113c347c8ec5420
semp26 <semp26.msemp26.mc>
2024-11-19 07:35:58 +0100

    Teile vom Init erstellt

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-# a link defines relation between 2 nodes
-class Link:
-    def __init__(self):
-        return
+import sys
 
-# Colum houses nodes
-class Colum:
-    def __init__(self):
-        return
+
+nodeNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nodelist = []
+
 
 # definition of a standard node
 class Node:
-    __col: [Colum]
+    def __init__(self, name):
+        self.incomingConnections = []
+        self.nodeName = name
 
-    def __init__(self):
-        return
+    def newconnection(self, incomingnode):
+        self.incomingConnections.append(incomingnode)
 
-def main():
-    return
+    def debugnode(self):
+        print("Debug of Node ", self.nodeName, ":")
+        print(self.incomingConnections)
+        print("--- End of Debug for Node ", self.nodeName, " ---")
+
+
+
+def initnodes():
+    global dampeningFactor
+    dampeningFactor = float(input("Was ist dein 'd' Wert?"))
+    print("Debug: dampeningFactor = ", dampeningFactor) if debug else None
+    nodeAmount = int(input("Wie viele Nodes gibt es? (>= 2 & <= 26)"))
+
+    for i in range(nodeAmount):
+        nodelist.append(Node(nodeNames[i]))
+
+    for node in nodelist:
+        node.debugnode()
+
+
+
+if __name__ == '__main__':
+    if "-debug" in sys.argv:
+        debug = True
+        print("Debug mode enabled")
+
+    initnodes()
