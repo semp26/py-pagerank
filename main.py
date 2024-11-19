9088ddf2740d90c009e4df06de4c703430898493
semp26 <semp26.msemp26.mc>
2024-11-19 08:08:37 +0100

    Node Verknüpfungen in eigene Funktion ausgelagert. Kommentare, TODO's und Nice to Haves (NTH) hinzugefügt.

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ class Node:
 
 def initnodes():
     global dampeningFactor
-    global nodelist
-
+    global nodelist # Die Node Instanzen werden in einer globalen Liste gespeichert
+    # User gibt den dampening Faktor d an TODO: input validation + error handling
     dampeningFactor = float(input("Was ist dein 'd' Wert?"))
     print("Debug: dampeningFactor = ", dampeningFactor) if debug else None
+
+    # User gibt an, wie viele Nodes es gibt.
+    # Aktuell ist 26 das maximum, da die Nodes von A bis Z benannt werden sollte aber 100 % ausreichen
     nodeamount = int(input("Wie viele Nodes gibt es? (>= 2 & <= 26)"))
     for i in range(nodeamount):
         nodelist.append(Node(nodeNames[i]))
@@ -36,6 +39,7 @@ def initnodes():
 
 
 def initconnections(newnodes):
+    # TODO: input validation + error handling
     for node in newnodes:
         connectionamount = int(input("Wie viele eingehende Verbindungen hat Node '" + node.nodeName + "'"))
         for j in range(connectionamount):
@@ -45,9 +49,15 @@ def initconnections(newnodes):
 
 
 if __name__ == '__main__':
+    # Debug mode, wenn das Program mit der Flag "-debug" aufgerufen wird
     if "-debug" in sys.argv:
         debug = True
         print("Debug mode enabled")
 
     nodelist = initnodes()
     initconnections(nodelist)
+    # TODO: ermitteln, wie oft der Pagerank iterativ berechnet werderen soll
+    # TODO: Iterative Berechnung des Pageranks -> Ergebnisse in einem 2D Array speichern
+    # TODO: Formatierte ausgabe der Ergebnisse
+    # NTH: Möglichkeit die Eingabe nachzubessern
+    # NTH: Nicht lineare Menüführung
